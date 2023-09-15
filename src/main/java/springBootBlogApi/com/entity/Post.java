af60001cc31821c4c5dbbc8b14a1b36a4833a94a
hamidie00235 <ahmathisseinh@gmail.com>
2023-09-16 02:57:26 +0300

    git commit -m "first commit"

---
+++ src/main/java/springBootBlogApi/com/entity/Post.java
@@ -0,0 +1,23 @@
+package springBootBlogApi.com.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table( name="posts" ,uniqueConstraints = {@UniqueConstraint(columnNames={"title"})})
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+@Column(name = "title" ,nullable = false)
+    private String title;
+    @Column(name = "description" ,nullable = false)
+    private String description;
+    @Column(name = "content",nullable = false)
+    private String content;
+}
---
+++ src/test/java/springBootBlogApi/com/SpringBootBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package springBootBlogApi.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
