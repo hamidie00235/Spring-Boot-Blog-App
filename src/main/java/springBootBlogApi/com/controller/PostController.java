dd819c8c2bdb740b618245041fdb1bd587d08578
hamidie00235 <ahmathisseinh@gmail.com>
2023-09-23 01:30:26 +0300

    Refactoring Hardcoded Values for Paging and Sorting

--- src/main/java/springBootBlogApi/com/controller/PostController.java
+++ src/main/java/springBootBlogApi/com/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import springBootBlogApi.com.payLoad.PostDto;
 import springBootBlogApi.com.payLoad.PostResponse;
 import springBootBlogApi.com.service.PostService;
+import springBootBlogApi.com.utils.AppConstants;
 
 import java.util.List;
 
@@ -25,10 +26,10 @@ public class PostController {
     // get all posts and pagination step1
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value ="pageNo" ,defaultValue = "0",required = false) int pageNo,
-            @RequestParam(value="pageSize" ,defaultValue = "10" ,required = false) int pageSize,
-            @RequestParam(value="sortBy",defaultValue = "id",required = false) String sortBy,
-            @RequestParam(value="sortDir",defaultValue="asc",required=false) String sortDir
+            @RequestParam(value ="pageNo" ,defaultValue = AppConstants.DEFAULT_PAGE_NUMBER,required = false) int pageNo,
+            @RequestParam(value="pageSize" ,defaultValue = AppConstants.DEFAULT_PAGE_SIZE ,required = false) int pageSize,
+            @RequestParam(value="sortBy",defaultValue = AppConstants.DEFAULT_PAGE_SORT_BY,required = false) String sortBy,
+            @RequestParam(value="sortDir",defaultValue=AppConstants.DEFAULT_PAGE_SORT_DIRECTION,required=false) String sortDir
     ){
         return postService.getAllPosts( pageNo ,pageSize,sortBy,sortDir);
     }
