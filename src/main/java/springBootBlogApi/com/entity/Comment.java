458f6fc0523e799b2a468757e161e78d1c22ca23
hamidie00235 <ahmathisseinh@gmail.com>
2023-09-25 21:55:31 +0300

    Comment Repository jpa is created

--- src/main/java/springBootBlogApi/com/entity/Comment.java
+++ src/main/java/springBootBlogApi/com/entity/Comment.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private String email;
     private String body;
---
+++ src/main/java/springBootBlogApi/com/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package springBootBlogApi.com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import springBootBlogApi.com.entity.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+}
