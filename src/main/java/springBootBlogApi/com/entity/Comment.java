8f91b921717560cf945918da853381d82d073da4
hamidie00235 <ahmathisseinh@gmail.com>
2023-09-25 20:38:32 +0300

    Comment jpa Entity is created

---
+++ src/main/java/springBootBlogApi/com/entity/Comment.java
@@ -0,0 +1,26 @@
+package springBootBlogApi.com.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String body;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+
+    @JoinColumn(name = "post_id" ,nullable=false)
+
+    private Post post;
+}
--- src/main/java/springBootBlogApi/com/entity/Post.java
+++ src/main/java/springBootBlogApi/com/entity/Post.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,4 +23,9 @@ public class Post {
     private String description;
     @Column(name = "content",nullable = false)
     private String content;
+
+    @OneToMany(mappedBy = "post",cascade =CascadeType.ALL,orphanRemoval = true)
+ private  Set<Comment> comments=new HashSet<>();
+
+
 }
