baea27f2027abda8508d5391f259f594a10c9cd4
hamidie00235 <ahmathisseinh@gmail.com>
2023-09-30 20:55:27 +0300

    Refactoring GetPostById and GetAllPosts REST API

--- src/main/java/springBootBlogApi/com/entity/Post.java
+++ src/main/java/springBootBlogApi/com/entity/Post.java
@@ -1,14 +1,13 @@
 package springBootBlogApi.com.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
--- src/main/java/springBootBlogApi/com/payLoad/PostDto.java
+++ src/main/java/springBootBlogApi/com/payLoad/PostDto.java
@@ -2,10 +2,13 @@ package springBootBlogApi.com.payLoad;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDto {
     private Long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
