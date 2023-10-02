e1808db50519c5886ab5752b50d454c14f4878dc
hamidie00235 <ahmathisseinh@gmail.com>
2023-10-02 23:41:39 +0300

    Customizing Validation Response

--- src/main/java/springBootBlogApi/com/exception/GlobalExceptionHandler.java
+++ src/main/java/springBootBlogApi/com/exception/GlobalExceptionHandler.java
@@ -1,16 +1,26 @@
 package springBootBlogApi.com.exception;
 
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import springBootBlogApi.com.payLoad.ErrorDetails;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 
 
@@ -41,4 +51,35 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    // method 1 customizing validation response
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String,String> errors=new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName=((FieldError)error).getField();
+            String message= error.getDefaultMessage();
+            errors.put(fieldName,message);
+
+        });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    }
+    // method 2 customizing validation response
+
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception,
+//                                                                              WebRequest webRequest){
+//
+//            Map<String,String> errors=new HashMap<>();
+//            exception.getBindingResult().getAllErrors().forEach((error)-> {
+//                String fieldName = ((FieldError) error).getField();
+//                String message = error.getDefaultMessage();
+//                errors.put(fieldName, message);
+//            });
+//        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
+
+
 }
