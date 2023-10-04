c4f7d5ae034ba44d2d40f59162deb4434c847722
hamidie00235 <ahmathisseinh@gmail.com>
2023-10-04 16:15:22 +0300

    Handling AccessDeniedException to Return UNAUTHORIZED Status

--- src/main/java/springBootBlogApi/com/exception/GlobalExceptionHandler.java
+++ src/main/java/springBootBlogApi/com/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -82,4 +83,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 
 
-}
+
+//denied method
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDetails> handleAccessDeniedException(AccessDeniedException exception, WebRequest webRequest){
+        ErrorDetails errorDetails=new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }}
