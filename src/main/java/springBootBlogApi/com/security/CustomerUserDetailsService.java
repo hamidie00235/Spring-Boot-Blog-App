fb03744366d2571c5758dc3e065eb5b8a1c41f00
hamidie00235 <ahmathisseinh@gmail.com>
2023-10-05 15:58:16 +0300

    UserDetailsService Interface Implementation - CustomUserDetailsService

---
+++ src/main/java/springBootBlogApi/com/security/CustomerUserDetailsService.java
@@ -0,0 +1,29 @@
+package springBootBlogApi.com.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import springBootBlogApi.com.entity.User;
+import springBootBlogApi.com.repository.UserRepository;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CustomerUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomerUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+       User user= userRepository.findAllByUserNameOrEmail(usernameOrEmail,usernameOrEmail)
+                .orElseThrow(()->new UsernameNotFoundException("user not found with username or email"+usernameOrEmail));
+        Set<GrantedAuthority> authorities =user.getRoles().stream().map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),authorities);
+    }
+}
